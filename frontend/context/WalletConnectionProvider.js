1f5c9ba311c00a9be8f6ae667058e17fdde2f763
AP696 <addyphilips66@gmail.com>
2022-11-04 23:56:35 +0000

    1

---
+++ frontend/context/WalletConnectionProvider.js
@@ -0,0 +1,26 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+
+const connection =
+  "https://frosty-neat-snow.solana-devnet.discover.quiknode.pro/57928ef58276ddb4bd5558b84d829aac8bcd78ea/";
+
+const WalletConnectionProvider = ({ children }) => {
+  const endpoint = useMemo(() => connection, []);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletConnectionProvider;
